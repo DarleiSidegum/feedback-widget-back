5b694a208f27e49ad4ca7d52a686660e79bd7078
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-05-10 18:04:37 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
+import { prisma } from './prisma';
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+app.post('/feedbacks', (req, res) => {
+    const { type, commment, screenshot } = req.body
+    prisma.feedback.create({
+        data: {
+            type,
+            commment,
+            screenshot,
+        }
+    })
+    return res.send('Hellow World')
+})
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
     console.log('HTTP server running!');
 })
